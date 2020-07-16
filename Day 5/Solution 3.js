c2bb039c33571c4895a323fc95fe76415ecc4897
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-07-17 03:22:30 +0530

    Solution 3

---
+++ Day 5/Solution 3.js
@@ -0,0 +1,34 @@
+let mark = prompt('enter your mark');
+
+if(mark>=90 && mark<=100){
+  console.log('A+');}
+else if(mark>=85 && mark<=89){
+  console.log('A');
+}
+else if(mark>=80 && mark<= 84){
+  console.log('A-');
+}
+else if(mark>=75 && mark<=79){
+  console.log('B+');
+}
+else if(mark>=70 && mark<=74){
+  console.log('B');
+}
+else if(mark>=65 && mark<=69){
+  console.log('C+');
+}
+else if(mark>=60 && mark<=64){
+  console.log('C');
+}
+else if(mark>=55 && mark<=59){
+  console.log('B+');
+}
+else if(mark>=50 && mark<=54){
+  console.log('D');
+}
+else if(mark>40 && mark<=49){
+  console.log('E');
+}
+else if(mark>=0 && mark<=39){
+  console.log('F');
+}
