32c3acbc67aae6b2a2655de425ed931fef56cc34
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-07-17 02:57:15 +0530

    Solution 1

---
+++ Day 5/Solution 1.js
@@ -0,0 +1,13 @@
+let number = prompt("enter any number");
+
+isEven(number);
+
+function isEven(number){
+
+  if(number%2==0){
+    console.log('even')
+  }
+  else{
+    console.log('odd');
+  }
+}
