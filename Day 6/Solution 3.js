1637444725db16080daf1efc0b7eba9b82f53666
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-07-18 01:26:29 +0530

    Solution 3

---
+++ Day 6/Solution 3.js
@@ -0,0 +1,7 @@
+var shoppingList=['a','b','c'];
+console.log(shoppingList);
+var shoppingBucket = shoppingList;
+shoppingBucket.push('d');
+shoppingBucket.push('e');
+shoppingBucket.push('f');
+console.log(shoppingBucket);
