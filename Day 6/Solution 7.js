14d4107c7335541704d0b0673023deaa9795c2e4
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-07-20 01:25:55 +0530

    Solution 7

---
+++ Day 6/Solution 7.js
@@ -0,0 +1,22 @@
+let userInput = Number(prompt("Enter A Number"));
+checkPrime(userInput);
+
+function checkPrime(n){
+let i = 2;
+let count,c = null;       
+   for (count = 2 ;count <= n;  )
+   {
+      for (c = 2 ;c <= i - 1;c++)
+      {
+         if (i%c == 0)
+            break;
+      }
+      if (c == i)
+      {
+        console.log(i);
+        document.getElementById("demo").innerHTML += i+"<br>";
+         count++;
+      }
+      i++; 
+}
+}   
