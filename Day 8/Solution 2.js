eeff2d05e2a26939c9258388c4e45360bea1c085
HariSuriya520 <59723129+HariSuriya520@users.noreply.github.com>
2020-07-22 04:21:47 +0530

    Solution 2

---
+++ Day 8/Solution 2.js
@@ -0,0 +1,22 @@
+let userInput = Number(prompt("Enter A Number "));
+let tables = null;
+let pTables = document.getElementById('ptable');
+let h3Tables = document.getElementById('h3table');
+
+//Styling of WebPage
+
+h3Tables.style.fontSize = "25px";
+pTables.style.fontSize = "20px";
+pTables.style.marginLeft = "20px";
+h3Tables.style.marginLeft = "10px";
+console.log(userInput !=="");
+
+//Logic TO Print Tables 
+if(userInput !=null && userInput !=""){
+    h3Tables.innerText = `Table of ${userInput} :-`;
+    for(i=1;i<=12;i++){
+        tables = `${userInput} x ${i} = ${userInput*i}<br>`;
+        // console.log(tables);
+        pTables.innerHTML += tables;
+    }
+}
